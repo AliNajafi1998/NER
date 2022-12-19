ce3fda3e1d8c2dc28b50aa6dce2385abcb4af3ed
Ali Najafi <najafi1998ali@gmail.com>
2022-12-19 20:19:11 +0300

    Update train.py

--- NER/train.py
+++ NER/train.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     param_optimizer = list(model.named_parameters())
 
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    optimizer_parameter = {
+    optimizer_parameter = [
         {
             "params": [
                 p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
@@ -84,7 +84,7 @@ if __name__ == "__main__":
             ],
             "weight_decay": 0.0,
         },
-    }
+    ]
 
     num_train_steps = int(
         len(train_sentences) / config.TRAIN_BATCH_SIZE * config.EPOCHS
